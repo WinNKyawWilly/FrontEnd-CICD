f5e6408a8c3cbe1d0ee59a1711f996fdec6cd71f
WinNKyawWilly <winnaingkyaw_win@cmu.ac.th>
2024-03-09 16:35:12 +0700

    Debugging Refusing to Connect

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! Testing');
 });
 
 app.listen(3000, () => {
